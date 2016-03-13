586b88d292b71905820113085c63bb4a298c8ca0
jlorenc <jonathan.lorenc@gmail.com>
2016-03-13 16:18:24 -0600

    Moved type of track to its own column

--- app/models/track.js
+++ app/models/track.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   source: DS.attr(),
+  type:DS.attr(),
   iscollected: DS.attr('boolean', { defaultValue: false })
 
 });
