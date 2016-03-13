4c6c7340c958baf4d38ec7bd22c27c67ca4d844b
jlorenc <jonathan.lorenc@gmail.com>
2016-03-13 00:30:53 -0600

    Only change baseUrl in production

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ffxiv-orchestrion',
     environment: environment,
-    baseURL: '/ffxiv',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+      ENV.baseURL = '/ffxiv';
   }
 
   return ENV;
